11592ab1a3e5add9f11f7acf6c1031a81d19d46a
Julia <tsapikova@mail.ru>
2022-12-21 14:08:39 +0400

    Task 10

---
+++ task_10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10. Написать программу, которая на вход принимает трехзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+int GetSecondDigit(int number)
+{
+    int number1 = number % 100;
+    int number2 = number1 / 10;
+    return number2;
+}
+
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Вторая цифра трехзначного числа равна {GetSecondDigit(number)}");
