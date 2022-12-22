1e2d5a39305ea993c8e45e23ab5b1f40dc335f54
Julia <tsapikova@mail.ru>
2022-12-22 15:38:08 +0400

    Final version

---
+++ task_13/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+void GetThirdDigit(double number)
+{
+    if(number < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+        while (number > 999)
+			{
+				number = Math.Truncate(number/10);
+			}
+			
+		double delta = number - Math.Truncate(number/10)*10;
+        Console.WriteLine($"Ответ: {delta}");
+    }
+		
+}
+Console.Write("Введите число: ");
+double number = double.Parse(Console.ReadLine()!);
+GetThirdDigit(number);
---
+++ task_15/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void IsWeekend(int number)
+{  
+    if(number == 6 || number == 7)
+    {
+    Console.WriteLine("да");
+    }
+    else
+    {
+    Console.WriteLine("нет");
+    }
+}
+Console.Write("Введите число от 1 до 7: ");
+int number = int.Parse(Console.ReadLine()!);
+IsWeekend(number);
